b20e36f59376d68ae67e6f318359618d40fd0328
Pedro Marques <pedrojmarques@deloitte.pt>
2025-01-14 11:13:20 +0000

    complementing network suggestions

--- src/android/wifiwizard2/WifiWizard2.java
+++ src/android/wifiwizard2/WifiWizard2.java
@@ -724,6 +724,23 @@ public class WifiWizard2 extends CordovaPlugin {
                 // Handle error
                 callbackContext.error("STATUS_NETWORK_SUGGESTIONS_SUCCESS");
             }
+
+            // Optional (Wait for post connection broadcast to one of your suggestions)
+            final IntentFilter intentFilter
+                    = new IntentFilter(WifiManager.ACTION_WIFI_NETWORK_SUGGESTION_POST_CONNECTION);
+
+            final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    if (!intent.getAction().equals(
+                            WifiManager.ACTION_WIFI_NETWORK_SUGGESTION_POST_CONNECTION)) {
+                        return;
+                    }
+                    // do post connect processing here...
+                    Log.d(TAG, "POST CONNECT ACTION");
+                }
+            };
+            Context.registerReceiver(broadcastReceiver, intentFilter);
         } else {
             // Fallback for older devices
             Log.d(TAG, "WifiWizard2: Fallback for older devices. Enabling connection...");
